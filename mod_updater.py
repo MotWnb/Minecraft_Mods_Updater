1cd7422d330bfd93c74ec3b91df1703a06f49495
motwnb <yangxvanming@126.com>
2023-06-25 09:07:59 +0800

    update

--- mod_updater.py
+++ mod_updater.py
@@ -1,9 +1,46 @@
+import fnmatch
 import os
 import json
-import fnmatch
-import zipfile
 import tempfile
-import simplejson as json
+import zipfile
+
+curseforge_new = []
+curseforge_old = []
+modrinth = []
+github = []
+
+def process_fabric_mod_json(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        try:
+            data = json.loads(f.read())
+        except json.JSONDecodeError as e:
+            print(f"JSON解析错误:{e}")
+            return
+
+    homepage = data.get("contact", {}).get("homepage")
+    sources = data.get("contact", {}).get("sources")
+
+    if homepage is not None and "www.curseforge.com" in homepage:
+        curseforge_new.append({"homepage": homepage})
+    elif homepage is not None and "minecraft.curseforge.com" in homepage:
+        curseforge_old.append({"homepage": homepage})
+    elif homepage is not None and "modrinth.com" in homepage:
+        modrinth.append({"homepage": homepage})
+    elif homepage is not None and "github.com" in homepage:
+        github.append({"homepage": homepage})
+
+    if sources is not None and "www.curseforge.com" in sources:
+        curseforge_new.append({"sources": sources})
+    elif sources is not None and "minecraft.curseforge.com" in sources:
+        curseforge_old.append({"sources": sources})
+    elif sources is not None and "modrinth.com" in sources:
+        modrinth.append({"sources": sources})
+    elif sources is not None and "github.com" in sources:
+        github.append({"sources": sources})
+
+        # 如果你想获取更多信息，你可以在这里继续处理 sources 字段
+        # ...
+
 
 def unzip(path, type):
     # 指定要解压的 .jar 文件路径
@@ -14,32 +51,15 @@ def unzip(path, type):
         # 解压 .jar 文件中的所有文件
         temp_dir = tempfile.mkdtemp()
         zip_ref.extractall(temp_dir)
-    
+
     if type == 'Fabric':
-    # 检查临时文件夹中是否存在文件 fabric.mod.json
+        # 检查临时文件夹中是否存在文件 fabric.mod.json
         fabric_mod_json = os.path.join(temp_dir, "fabric.mod.json")
         if os.path.exists(fabric_mod_json):
             print("找到fabric.mod.json")
-            with open(fabric_mod_json, "r", encoding="utf-8") as f:
-                try:
-                    data = json.loads(f.read())
-                except json.JSONDecodeError as e:
-                    print(f"JSON解析错误：{e}")
-                    return
-
-            homepage = data.get("contact", {}).get("homepage")
-            sources = data.get("contact", {}).get("sources")
-
-            # 输出结果
-            if homepage:
-                print("homepage:", homepage)
-            if sources:
-                print("sources:", sources)
-                
-
+            process_fabric_mod_json(fabric_mod_json)
         else:
             print("未找到fabric.mod.json")
-
 
     for root, dirs, files in os.walk(temp_dir, topdown=False):
         for name in files:
@@ -49,16 +69,23 @@ def unzip(path, type):
     os.rmdir(temp_dir)
 
 
-
 def upgrade_mods(path, type):
-    # 在这里添加您更新 Minecraft 模组的代码
-    # print(f"正在更新版本为 '{path}' 的 Minecraft 模组...")
     for dirpath, dirname, filenames in os.walk(path):
-        # 遍历当前目录下的所有文件
         for filename in filenames:
-            # 判断文件名是否以 .jar 结尾
             if fnmatch.fnmatch(filename, "*.jar"):
-                # 如果是 .jar 文件，则输出文件路径
                 print('发现一个mod:', filename)
                 filepath = os.path.join(dirpath, filename)
                 unzip(filepath, type)
+
+    # 将分类后的模组信息写入文件
+    with open("curseforge_new.json", "w", encoding="utf-8") as f:
+        json.dump(curseforge_new, f, ensure_ascii=False, indent=2)
+
+    with open("curseforge_old.json", "w", encoding="utf-8") as f:
+        json.dump(curseforge_old, f, ensure_ascii=False, indent=2)
+
+    with open("modrinth.json", "w", encoding="utf-8") as f:
+        json.dump(modrinth, f, ensure_ascii=False, indent=2)
+
+    with open("github.json", "w", encoding="utf-8") as f:
+        json.dump(github, f, ensure_ascii=False, indent=2)
